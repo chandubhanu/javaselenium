f2234442fe223537ccb12d128e1c490d4e00cd96
Bhanu <chandubhanuprakash123@gmail.com>
2024-07-29 18:25:58 +0530

    Intial Commit

---
+++ src/javaselenium/firstcode.java
@@ -0,0 +1,13 @@
+package javaselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webDriver.chrome.driver","C://chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
